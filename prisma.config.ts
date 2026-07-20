b9dc3a7bca175733b53372f4bacfc83140272acf
Md Rubel Hosen <rubelhosen1310@gmail.com>
2026-07-20 12:53:06 +0600

    feat: initialize modular Prisma schema structure with product and identity models

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema",
+  schema: "prisma",
   datasource: {
     url: env("DATABASE_URL"),
   },
