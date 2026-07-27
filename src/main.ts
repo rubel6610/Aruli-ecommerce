91a283aa96e8ab5bba3866cf5c9b6902cd6994ff
Md Rubel Hosen <rubelhosen1310@gmail.com>
2026-07-27 06:10:28 +0600

    feat: bootstrap NestJS application with global validation pipe and environment configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
+/* eslint-disable prettier/prettier */
 import 'dotenv/config'; // Parse and load environment variables first
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
